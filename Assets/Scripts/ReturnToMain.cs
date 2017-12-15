8dd3ef6362ba014db8a065593bf6aba115323c97
Lim Hyun (임현) <hyunzion@gmail.com>
2017-12-15 17:41:29 +0900

    Revert "ReturnToMain.cs 추가"
    This reverts commit 47bdbb47b31bed22c80e6de3e881327c193732fb.

--- Assets/Scripts/ReturnToMain.cs
+++ Assets/Scripts/ReturnToMain.cs
@@ -1,11 +1,4 @@
-﻿ /**
-  * @file : ReturnToMain.cs
-  * @author : 임현 (hyunzion@gmail.com)
-  * @since : 2017 - 12 - 15
-  * @brief : Main(Morning_Running) Scene으로 돌아감
-  */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
