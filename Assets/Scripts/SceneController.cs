c1a9bc5a9ea4379a1c86badc7a09576bc57ccc6a
Lim Hyun (임현) <hyunzion@gmail.com>
2017-12-16 22:15:43 +0900

    SceneController.cs 추가
      * @file : SceneController.cs
      * @author : 임현 (hyunzion@gmail.com)
      * @since : 2017 - 12 - 16
      * @brief : Scene들을 관리
    
    ReturnToMain.cs 를 삭제하고 통합함

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,34 @@
+﻿/**
+  * @file : SceneController.cs
+  * @author : 임현 (hyunzion@gmail.com)
+  * @since : 2017 - 12 - 16
+  * @brief : Scene들을 관리
+  */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+	public void ReturnMain()
+	{
+		SceneManager.LoadScene("Morning_Running");
+	}
+
+	public void ReturnBicycle()
+	{
+		SceneManager.LoadScene("Bicycle");
+	}
+
+	public void ReturnSetUp()
+	{
+		SceneManager.LoadScene("SetUp");
+	}
+
+	public void ReturnStory()
+	{
+		SceneManager.LoadScene("Story");
+	}
+}
