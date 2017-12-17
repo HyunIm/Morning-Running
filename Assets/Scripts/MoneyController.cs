d8b6eb4a439d088fb498a5492965644e28b0293e
Lim Hyun (임현) <hyunzion@gmail.com>
2017-12-17 13:38:45 +0900

    복귀 버튼 구현
    Comeback 함수 추가

--- Assets/Scripts/MoneyController.cs
+++ Assets/Scripts/MoneyController.cs
@@ -13,40 +13,64 @@ using UnityEngine.UI;
 public class MoneyController : MonoBehaviour
 {
 	public char gold_unit = 'a';
-	public char credit_unit = 'a';
+	public int credit_unit = 0;
 	public int gold = 1;
 	public int credit = 1;
+	public int time = 0;
+	int major = 1;
 
 	public Text goldLabel;
 	public Text creditLabel;
 
-	private void Update()
+	private void Start()
 	{
-		SendMessage("CalcGold");
-		SendMessage("CalcCredit");
-	}
-
-	public void CalcGold (int gold)
-	{
-		gold++;
-
-		GoldRoundDown(gold);
-		goldLabel.text = " " + gold + gold_unit;
-	}
-
-	public void CalcCredit (int credit)
-	{
+		goldLabel.text = "" + gold + gold_unit;
 		creditLabel.text = " " + credit + credit_unit;
 	}
 
+	private void Update()
+	{
+		time++;
+		if (time == 1)
+		{
+			gold += major;
+			time = 0;
+		}
+		GoldRoundDown(gold);
+		goldLabel.text = "" + gold + gold_unit;
+		creditLabel.text = " " + credit;
+	}
+
+	/// <summary>
+	/// @title : int GoldRoundDown (int gold)
+	/// @param : int gold (골드 값)
+	/// @return : 1000이 넘을 시 1을 반환, 아닐 시 패스
+	/// @brief : 화폐 단위를 잘라주는 함수 (a부터 z까지)
+	/// </summary>
 	int GoldRoundDown (int gold)
 	{
 		if (gold == 1000)
-		{
+		{	
 			gold_unit++;
-			return gold - 1000;
+			this.gold = 1;
+			return 1;
 		}
 
 		return gold;
+	}
+
+	/// <summary>
+	/// @title : public void ComeBack ()
+	/// @brief : 복귀 버튼 (돈 초기화, 학점 증가)
+	/// </summary>
+	public void ComeBack ()
+	{
+		// char 'a'(97) 에서 빼줌
+		credit_unit = gold_unit - 97;
+
+		this.credit += ((gold + (credit_unit * 1000)) / 1000);
+
+		gold = 0;
+		gold_unit = 'a';
 	}
 }
