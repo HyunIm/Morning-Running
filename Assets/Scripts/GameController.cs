f59aec4879c4895f65766c3ef4b92047be7e2064
Lim Hyun (임현) <hyunzion@gmail.com>
2017-12-17 17:12:46 +0900

    GameController.cs 통합
     * @file : GameController.cs
     * @author : 임현 (hyunzion@gmail.com)
     * @since : 2017 - 12 - 17
     * @brief : 게임 컨트롤러 (화폐, 복귀, 장비, 특성, 스킬)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,16 @@
-﻿ /**
-  * @file : MoneyController.cs
-  * @author : 임현 (hyunzion@gmail.com)
-  * @since : 2017 - 12 - 16
-  * @brief : 화폐 컨트롤러
-  */
+﻿/**
+ * @file : GameController.cs
+ * @author : 임현 (hyunzion@gmail.com)
+ * @since : 2017 - 12 - 17
+ * @brief : 게임 컨트롤러 (화폐, 복귀, 장비, 특성, 스킬)
+ */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MoneyController : MonoBehaviour
+public class GameController : MonoBehaviour
 {
 	/// <summary>
 	/// 화폐
@@ -60,10 +60,10 @@ public class MoneyController : MonoBehaviour
 	/// @return : 1000이 넘을 시 1을 반환, 아닐 시 패스
 	/// @brief : 화폐 단위를 잘라주는 함수 (a부터 z까지)
 	/// </summary>
-	int GoldRoundDown (int gold)
+	int GoldRoundDown(int gold)
 	{
-		if (gold == 1000)
-		{	
+		if (gold >= 1000)
+		{
 			gold_unit++;
 			this.gold = 1;
 			return 1;
@@ -76,8 +76,9 @@ public class MoneyController : MonoBehaviour
 	/// @title : public void ComeBack ()
 	/// @brief : 복귀 버튼 (돈 초기화, 학점 증가)
 	/// </summary>
-	public void ComeBack ()
+	public void ComeBack()
 	{
+		print("ComeBack");
 		// char 'a'(97) 에서 빼줌
 		credit_unit = gold_unit - 97;
 
@@ -86,4 +87,55 @@ public class MoneyController : MonoBehaviour
 		gold = 0;
 		gold_unit = 'a';
 	}
+
+
+	int name_credit = 1000;
+	int major_credit = 10;
+	int grade_credit = 100;
+
+	public void ReName()
+	{
+		print("ReName");
+		if (credit > name_credit)
+		{
+			credit -= name_credit;
+			name_credit += 1000;
+			name++;
+		}
+	}
+
+	public void ReMajor()
+	{
+		print("ReMajor");
+		if (credit > major_credit)
+		{
+			credit -= major_credit;
+			major_credit += 10;
+			major++;
+		}
+	}
+
+	public void ReGrade()
+	{
+		print("ReGrade");
+		if (credit > grade_credit)
+		{
+			credit -= grade_credit;
+			grade_credit += 10;
+			grade++;
+		}
+	}
+
+
+	public void Water()
+	{
+		gold += 1000;
+	}
+
+	public void Breath()
+	{
+		credit += 10;
+	}
+
+
 }
