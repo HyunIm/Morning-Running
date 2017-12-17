b8eff09569c5f9db6471f62183744310242042a6
Lim Hyun (임현) <hyunzion@gmail.com>
2017-12-18 00:47:27 +0900

    ProjectSettings 수정

---
+++ Assets/Scripts/ScreenController.cs
@@ -0,0 +1,18 @@
+﻿/**
+ * @file : ScreenController.cs
+ * @author : 임현 (hyunzion@gmail.com)
+ * @since : 2017 - 12 - 18
+ * @brief : 빌드 해상도 고정
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour
+{
+	private void Awake()
+	{
+		Screen.SetResolution(Screen.width, Screen.width * 9 / 16, false);
+	}
+}
