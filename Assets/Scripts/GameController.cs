5c08dba961b4ff7948b46ba544573d580d48d8e8
Lim Hyun (임현) <hyunzion@gmail.com>
2017-12-19 11:22:41 +0900

    GameController.cs 주석 추가
    설명을 돕기 위한 주석 추가

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,20 +15,20 @@ public class GameController : MonoBehaviour
 	/// <summary>
 	/// 화폐
 	/// </summary>
-	int gold = 1;
-	int credit = 1;
-	int time = 0;
+	int gold = 1;	// 골드
+	int credit = 1;	// 학점
+	int time = 0;	// 시간
 
 	/// <summary>
 	/// 장비
 	/// </summary>
-	int bag_level = 1;
-	int shoes_level = 1;
-	int bag1_price = 400;
+	int bag_level = 1;	// 가방 레벨
+	int shoes_level = 1;	// 신발 레벨
+	int bag1_price = 400;	// 가방 가격들
 	int bag2_price = 7000;
 	int bag3_price = 32000;
 	int bag4_price = 130000;
-	int shoes1_price = 100;
+	int shoes1_price = 100;	// 신발 가격들
 	int shoes2_price = 3000;
 	int shoes3_price = 15000;
 	int shoes4_price = 90000;
@@ -59,9 +59,9 @@ public class GameController : MonoBehaviour
 	int major = 1;	// 전과하기
 	int grade = 1;	// 학년증가
 	int rname = 1;   // 개명하기
-	int name_credit = 1000;
-	int major_credit = 10;
-	int grade_credit = 100;
+	int name_credit = 1000;	// 개명하기에 필요한 학점 가격
+	int major_credit = 10;	// 전과하기에 필요한 학점 가격
+	int grade_credit = 100;	// 학년증가에 필요한 학점 가격
 
 	/// <summary>
 	/// 스킬
@@ -71,16 +71,22 @@ public class GameController : MonoBehaviour
 	/// <summary>
 	/// UI
 	/// </summary>
-	public Text goldLabel;
-	public Text creditLabel;
+	public Text goldLabel;		// 골드 UI Text
+	public Text creditLabel;	// 학점 UI Text
 
 
+	/// <summary>
+	/// 시작하자마자 골드와 학점 UI를 표시함
+	/// </summary>
 	private void Start()
 	{
 		goldLabel.text = "" + gold;
 		creditLabel.text = " " + credit;
 	}
 
+	/// <summary>
+	/// 매 프레임마다 전공과 신발에 비례해서 골드가 오름
+	/// </summary>
 	private void Update()
 	{
 		time += shoes_level;
@@ -88,6 +94,8 @@ public class GameController : MonoBehaviour
 
 		if (time >= 100)
 		{
+			// 전공 레벨과 신발 레벨에 비례해서 오름
+			// 신발 레벨 비중이 더 높아야함
 			gold += 1 + ((major - 1) * 5) + ((shoes_level - 1) * 30);
 			time = 1;
 		}
@@ -104,9 +112,12 @@ public class GameController : MonoBehaviour
 	{
 		print("ComeBack");
 
+		// 골드에 비례해서 학점을 증가시켜줌
 		credit += (gold / (100 - (grade + (bag_level * 5))));
+		// 골드는 1로 초기화
 		gold = 1;
 
+		// 신발과 가방 레벨도 1로 초기화
 		bag_level = 1;
 		PreBag1.transform.localPosition = new Vector3(0, 0, 0);
 		PreBag2.transform.localPosition = new Vector3(-20, 0, 0);
@@ -132,14 +143,21 @@ public class GameController : MonoBehaviour
 		PostShoes5.transform.localPosition = new Vector3(-20, 0, 0);
 	}
 
-
+	/// <summary>
+	/// 가방 함수 (레벨1 ~ 레벨 5)
+	/// 가방 스프라이트의 위치를 바꾸고, 상점 역할을 수행
+	/// </summary>
 	public void Bag()
 	{
+		// 가방 레벨이 1일 경우
 		if (bag_level == 1)
 		{
+			// 골드가 가방 가격보다 많은지 확인
 			if (gold >= bag1_price)
 			{
+				// 골드에서 가방 가격만큼 빼고
 				gold -= bag1_price;
+				// 가방 레벨을 증가시켜줌
 				bag_level++;
 				PreBag1.transform.localPosition = new Vector3(-20, 0, 0);
 				PreBag2.transform.localPosition = new Vector3(0, 0, 0);
@@ -187,6 +205,10 @@ public class GameController : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// 신발 함수 (레벨1 ~ 레벨 5)
+	/// 신발 스프라이트의 위치를 바꾸고, 상점 역할을 수행
+	/// </summary>
 	public void Shoes()
 	{
 		if (shoes_level == 1)
@@ -241,7 +263,10 @@ public class GameController : MonoBehaviour
 		}
 	}
 
-
+	/// <summary>
+	/// 개명하기 함수
+	/// 스킬 쿨타임을 줄여줌
+	/// </summary>
 	public void ReName()
 	{
 		print("ReName");
@@ -252,6 +277,10 @@ public class GameController : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// 전과하기 함수
+	/// 골드 획득량을 증가
+	/// </summary>
 	public void ReMajor()
 	{
 		print("ReMajor");
@@ -262,6 +291,10 @@ public class GameController : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// 학년증가 함수
+	/// 학점 획득량을 증가
+	/// </summary>
 	public void ReGrade()
 	{
 		print("ReGrade");
@@ -273,6 +306,10 @@ public class GameController : MonoBehaviour
 	}
 
 
+	/// <summary>
+	/// 물 마시기 함수
+	/// 다량의 골드를 학과에 비례해서 줌
+	/// </summary>
 	public void Water()
 	{
 		if (cooldown_time <= 0)
@@ -282,6 +319,9 @@ public class GameController : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// 다량의 학점을 학년에 비례해서 줌
+	/// </summary>
 	public void Breath()
 	{
 		if (cooldown_time <= 0)
